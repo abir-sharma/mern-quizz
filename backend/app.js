d1794dc4398f0906beb37f3b2d5d6e0d981bee4a
abir-sharma <abirs7473@gmail.com>
2023-09-08 19:51:31 +0530

    v

--- backend/app.js
+++ backend/app.js
@@ -8,14 +8,14 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
-const errorMiddleware=require('./backend/middleware/error')
+const errorMiddleware=require("./middleware/error")
 
 if (process.env.NODE_ENV!=="PRODUCTION"){
     require("dotenv").config({path:"config/config.env"})
 }
 
-const user=require("./backend/routes/userRoute")
-const admin=require("./backend/routes/adminRoute")
+const user=require("./routes/userRoute")
+const admin=require("./routes/adminRoute")
 
 app.use("/api/v1",user)
 app.use("/api/v1",admin)
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app=require("./app")
-const connectDatabase=require("./backend/config/database")
+const connectDatabase=require("./config/database")
 process.on("uncaughtException",(err)=>{
     console.log(`Error: ${err.message}`)
     console.log(`Shutting down the server due to Uncaught Exception`)
